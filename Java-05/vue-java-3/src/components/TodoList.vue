20dd754b890493e98e15d78a4e31ce7240d35524
Jon Kursani <jonkursani@gmail.com>
2024-05-03 14:43:01 +0200

    Java 5 components

--- Java-05/vue-java-3/src/components/TodoList.vue
+++ Java-05/vue-java-3/src/components/TodoList.vue
@@ -1,26 +1,34 @@
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+import NewTodo from "@/components/NewTodo.vue";
 
 // const isDivRendered = ref(true);
 // const type = ref('D');
 
 
 const todos = ref([]);
-const todo = ref('');
+const inpTodo = ref('');
 
-function addTodo() {
+function addTodo(todo) {
+  if(!todo) {
+    return;
+  }
+
   todos.value.push({
-    id: todos.value.length + 1,
-    title: todo.value,
+    // id: todos.value.length + 1,
+    id: new Date().toISOString(),
+    title: todo,
     isCompleted: false
   })
 
-  todo.value = '';
+  inpTodo.value = ''
 }
 
-function removeTodo(index) {
-  // console.log(index);
-  todos.value.splice(index, 1);
+function removeTodo(id) {
+  if (confirm("Are you sure you want to delete this todo?")) {
+    todos.value = todos.value.filter((item) => item.id !== id);
+  }
 }
 
 function markAsDone(id) {
@@ -37,69 +45,45 @@ function markAsDone(id) {
       <h3>Todo list</h3>
     </div>
     <div class="card-body">
-      <div class="input-group mb-3">
-        <input type="text"
-               class="form-control"
-               placeholder="Add todo"
-               v-model.trim="todo"
-               @keyup.enter="addTodo">
-        <button class="btn btn-outline-success"
-                type="button"
-                @click="addTodo">
-          + Add
-        </button>
-      </div>
+
+      <NewTodo @addTodo="addTodo" v-model="inpTodo" />
 
       <ul class="list-group" v-if="todos.length > 0">
-        <li class="list-group-item d-flex justify-content-between"
-            v-for="(todo, index) in todos"
-            :key="todo.id"
-            :class="{ done: todo.isCompleted }">
-<!--          {{ index }} - -->
-          {{ todo.title }}
-
-          <span>
-            <button
-                class="btn btn-outline-success"
-                @click="markAsDone(todo.id)">
-              {{ todo.isCompleted ? 'Done' : 'Mark as done' }}
-            </button>
-            <button
-                class="btn btn-outline-danger ms-1"
-                @click="removeTodo(index)">
-              Delete
-            </button>
-          </span>
-        </li>
+        <!--        <todo-list-item id="" title="" />-->
+        <TodoListItem v-for="todo in todos"
+                      :key="todo.id"
+                      :id="todo.id"
+                      :title="todo.title"
+                      :isCompleted="todo.isCompleted"
+                      @removeTodo="removeTodo"
+                      @markAsDone="markAsDone"
+        />
       </ul>
       <h4 v-else class="text-center">No todos</h4>
     </div>
   </div>
 
-<!--  <button @click="isDivRendered = !isDivRendered">Toggle div</button>-->
-<!--  <div v-if="isDivRendered">-->
-<!--    If is rendered-->
-<!--  </div>-->
-<!--&lt;!&ndash;  <p></p>&ndash;&gt;-->
-<!--  <div v-else>-->
-<!--    else is rendered-->
-<!--  </div>-->
+  <!--  <button @click="isDivRendered = !isDivRendered">Toggle div</button>-->
+  <!--  <div v-if="isDivRendered">-->
+  <!--    If is rendered-->
+  <!--  </div>-->
+  <!--&lt;!&ndash;  <p></p>&ndash;&gt;-->
+  <!--  <div v-else>-->
+  <!--    else is rendered-->
+  <!--  </div>-->
 
 
-<!--  <div v-if="type === 'A'">A</div>-->
-<!--  <div v-else-if="type === 'B'">B</div>-->
-<!--  <div v-else-if="type === 'C'">C</div>-->
-<!--  <div v-else>Not A/B/C</div>-->
+  <!--  <div v-if="type === 'A'">A</div>-->
+  <!--  <div v-else-if="type === 'B'">B</div>-->
+  <!--  <div v-else-if="type === 'C'">C</div>-->
+  <!--  <div v-else>Not A/B/C</div>-->
 
-<!--  <div v-show="isDivRendered">Show is rendered</div>-->
+  <!--  <div v-show="isDivRendered">Show is rendered</div>-->
 
-<!--  v-show - v-if -->
-<!--  <div v-show=""></div>-->
+  <!--  v-show - v-if -->
+  <!--  <div v-show=""></div>-->
 </template>
 
 <style scoped>
-.done {
-  text-decoration: line-through;
-  color: green;
-}
+
 </style>
---
+++ Java-05/vue-java-3/src/components/TodoListItem.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+// defineProps(['id', 'title', 'isCompleted'])
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  isCompleted: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+const emit = defineEmits(['removeTodo', 'markAsDone']);
+
+function removeTodo(id) {
+  emit('removeTodo', id);
+}
+</script>
+
+<template>
+  <li class="list-group-item d-flex justify-content-between"
+      :class="{ done: isCompleted }">
+    <!--          {{ index }} - -->
+    <!--    {{ id }} -->
+    {{ title }}
+
+    <span>
+      <button
+          class="btn btn-outline-success"
+          @click="emit('markAsDone', id)">
+        {{ isCompleted ? 'Done' : 'Mark as done' }}
+      </button>
+      <button
+          class="btn btn-outline-danger ms-1"
+          @click="removeTodo(id)">
+        Delete
+      </button>
+    </span>
+  </li>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+  color: white;
+  background-color: lightgreen;
+}
+</style>
