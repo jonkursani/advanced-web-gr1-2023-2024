13e9b47687b16c628df5364c2aa6f9e2d57df740
Jon Kursani <jonkursani@gmail.com>
2024-05-08 20:40:31 +0200

    Java 7 components and router

---
+++ Java-07/vue-java-3/src/components/layout/TheHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  title: {
+    required: true,
+    type: String
+  }
+})
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ title }}</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+<!--            <router-link class="nav-link" to="/">Resources</router-link>-->
+<!--            <router-link class="nav-link" :to="{ path: '/' }">Resources</router-link>-->
+            <router-link class="nav-link" :to="{ name: 'resources' }">Resources</router-link>
+
+
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/add">Add resources</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
